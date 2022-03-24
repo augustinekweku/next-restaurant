e9d7cfa62c5434228ccd0ad4ed1d8c3925978941
augustinekweku <augustine.nani@gmail.com>
2022-03-24 16:11:13 +0000

    update cors in config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://next-restaurant-101.herokuapp.com/:path*",
+      },
+    ];
+  },
 };
